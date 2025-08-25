c74264edc9113f215a0f1adfd2acaf2f04ea3b62
Nikita Bulash <bulash2006@gmail.com>
2025-08-25 19:08:09 +0100

    Implementation of DAO + CRUD

---
+++ src/main/java/com/nikita/app/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package com.nikita.app.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.nikita.app.models.Person;
+
+@Component
+public class PersonDAO {
+  private List<Person> people;
+  private static int PEOPLE_COUNT;
+
+  {
+    people = new ArrayList<>();
+
+    people.add(new Person(++PEOPLE_COUNT, "Tom"));
+    people.add(new Person(++PEOPLE_COUNT, "Bob"));
+    people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    people.add(new Person(++PEOPLE_COUNT, "Katy"));
+  } 
+
+  public List<Person> index() {
+    return people;
+  }
+
+  public Person show(int id) {
+    return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+  }
+}
