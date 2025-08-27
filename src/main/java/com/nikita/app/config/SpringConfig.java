86597888279ae6cdd051dea433b6aae3737bb2ce
Nikita Bulash <bulash2006@gmail.com>
2025-08-27 19:56:57 +0100

    Implementation of JDBC Template

--- src/main/java/com/nikita/app/config/SpringConfig.java
+++ src/main/java/com/nikita/app/config/SpringConfig.java
@@ -1,10 +1,16 @@
 package com.nikita.app.config;
 
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -40,6 +46,22 @@ public class SpringConfig implements WebMvcConfigurer {
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
     }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/Java_DB");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("2124648306fF");
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
---
+++ src/main/java/com/nikita/app/models/PersonMapper.java
@@ -0,0 +1,18 @@
+package com.nikita.app.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class PersonMapper implements RowMapper<Person> {
+  @Override
+  public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+    Person person = new Person();
+    person.setId(rs.getInt("id"));
+    person.setName(rs.getString("name"));
+    person.setEmail(rs.getString("email"));
+    person.setAge(rs.getInt("age"));
+    return person;
+  }
+}
